2dfb4ca9679bd1696d7949aff13c70a88e700983
petems <p.morsou+GIT@gmail.com>
2019-10-17 15:51:35 +0300

    more money

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "foobar" {
   ami           = "ami-03ef731cc103c9f09" // eu-west-1 Ubuntu 16.04
-  instance_type = "t1.micro"
+  instance_type = "c5.metal"
 
   tags = {
     Name = "foobar"
